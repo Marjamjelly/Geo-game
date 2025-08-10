cddcd131bcac8f64174b510b8d5b9a592375c954
Marjamjelly <130711938+Marjamjelly@users.noreply.github.com>
2025-08-10 15:25:32 -0500

    Update levels.js

--- levels.js
+++ levels.js
@@ -2,9 +2,9 @@
 const levels = [
   {
     id: 1,
-    name: "World Countries",
+    name: "World Continents",
     map: "maps/world.svg",
-    questions: ["france", "brazil", "india", "canada", "australia"]
+    questions: ["north_america", "south_america", "europe", "asia", "africa", "australia"]
   },
   // Add more levels here
 ];
